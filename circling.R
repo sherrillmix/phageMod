b5b84debe22eadafc3d593750027de8bc1ffc1bc
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-05-01 22:35:21 -0400

    Add left turn functions. Remove extraneous column and duplicates. Sort plotting by turn counts

--- circling.R
+++ circling.R
@@ -1,3 +1,19 @@
+isLeft<-function(x1,x2,x3,y1,y2,y3){
+  cross<-(x2 - x1) * (y3- y1) - (y2 - y1) * (x3 - x1)
+  if(is.na(cross)||abs(cross)<1e-10)return(NA)
+  return(cross>0)
+}
+checkLeft<-function(xs,ys){
+  out<-rep(NA,length(xs))
+  if(length(xs)!=length(ys))stop(simpleError('xs and ys not same length'))
+  if(length(xs)<3)return(out)
+  for(ii in 3:length(xs)){
+    out[ii]<-isLeft(xs[ii-2],xs[ii-1],xs[ii],ys[ii-2],ys[ii-1],ys[ii]) 
+  }
+  return(out)
+}
+
+
 phage<-read.csv('data/phage_reads_for_abundance_analysis.csv',stringsAsFactors=FALSE)
 bac<-read.table('data/bacteria_16s_reads_for_abundance_analysis.tsv',stringsAsFactors=FALSE)
 
@@ -8,6 +24,7 @@ bac<-read.table('data/bacteria_16s_reads_for_abundance_analysis.tsv',stringsAsFa
 #)
 linkFiles<-list.files('data/link/','*.csv',full.names=TRUE)
 links<-do.call(rbind,lapply(linkFiles,read.csv,stringsAsFactors=FALSE))
+links<-links[,colnames(links)!='X']
 links<-unique(links[!grepl('Unclassified|Uncultured',links$Bacteria),])
 
 pCountCols<-colnames(phage)[grep('^X[0-9.]+$',colnames(phage))]
@@ -39,22 +56,26 @@ otuCounts<-do.call(rbind,lapply(bacPhage,sapply,nrow))
 
 pVsB<-lapply(bacPhage,function(x){
   lapply(oneDiffDays,function(days){
-    data.frame(
+    out<-data.frame(
       'phage'=apply(x[['phage']][,as.character(days),drop=FALSE],2,sum),
       'bac'=apply(x[['bac']][,as.character(days),drop=FALSE],2,sum)
     )
+    out$isLeft=checkLeft(out$bac,out$phage)
+    return(out)
   })
 })
+nLeft<-sapply(pVsB,function(x)sum(unlist(lapply(x,function(y)y[,'isLeft'])),na.rm=TRUE))
+nRight<-sapply(pVsB,function(x)sum(unlist(lapply(x,function(y)!y[,'isLeft'])),na.rm=TRUE))
+table(nLeft,nRight)
 
 
 cols<-rainbow.lab(3,lightScale=0,lightMultiple=.7,alpha=.6)
 pdf('out/circle.pdf')
-  lapply(1:nrow(links),function(ii){
+  lapply(order(nLeft-nRight),function(ii){
     thisInfo<-links[ii,]
     thisPB<-pVsB[[ii]]
     thisCat<-do.call(rbind,thisPB)
-    print(thisCat)
-    plot(thisCat$bac,thisCat$phage,main=sprintf('%s %s',thisInfo$Bacteria,thisInfo$contig),xlab='Bacteria proportion',ylab='Phage proportion',lwd=2,pch=21,bg=cols[rep(1:length(thisPB),sapply(thisPB,nrow))],col=NA,cex=2)
+    plot(thisCat$bac,thisCat$phage,main=sprintf('%s %s Lefts: %d Rights: %d',thisInfo$Bacteria,thisInfo$contig,nLeft[ii],nRight[ii]),xlab='Bacteria proportion',ylab='Phage proportion',lwd=2,pch=21,bg=cols[rep(1:length(thisPB),sapply(thisPB,nrow))],col=NA,cex=2)
     lapply(thisPB,function(x)arrows(x$bac[-nrow(x)],x$phage[-nrow(x)],x$bac[-1],x$phage[-1],length=.1))
   })
 dev.off()
